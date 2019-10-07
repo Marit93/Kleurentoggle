1900fd4ebb2cec0af17fd19a8d958f709e475d56
Marit G <maritgerritsen9@gmail.com>
2019-10-07 08:21:33 +0200

    Kleurentoggle first commit

---
+++ color.js
@@ -0,0 +1,20 @@
+
+  const menu = document.querySelector("menu");
+  const colors = document.querySelector("colors");
+
+  function functionHome(){
+    document.getElementById("body").style.backgroundColor = "white";
+  }
+  function functionRed(){
+    document.getElementById("body").style.backgroundColor = "red";
+  }
+  function functionOrange(){
+    document.getElementById("body").style.backgroundColor = "Orange";
+  }
+  function functionPurple(){
+    document.getElementById("body").style.backgroundColor = "purple";
+  }
+  function functionGreen(){
+    document.getElementById("body").style.backgroundColor = "green";
+  }
+  
